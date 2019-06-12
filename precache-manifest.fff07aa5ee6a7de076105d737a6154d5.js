75aefb1195aa84c54b8a2bef7dc8a85a24aa1daa
ndhoon <ndhooon@gmail.com>
2019-06-12 11:49:00 +0900

    Updates

---
+++ precache-manifest.fff07aa5ee6a7de076105d737a6154d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce99bfaa5ba67fb9b86e08c37eb8a482",
+    "url": "/banana03/index.html"
+  },
+  {
+    "revision": "dda0b4048175978e86a4",
+    "url": "/banana03/static/css/main.4da31322.chunk.css"
+  },
+  {
+    "revision": "b0c5012ca7746f497091",
+    "url": "/banana03/static/js/2.ab1b0ea4.chunk.js"
+  },
+  {
+    "revision": "dda0b4048175978e86a4",
+    "url": "/banana03/static/js/main.a7763edb.chunk.js"
+  },
+  {
+    "revision": "e0cf2f96a8807479c929",
+    "url": "/banana03/static/js/runtime~main.51fe0bf7.js"
+  }
+]);
